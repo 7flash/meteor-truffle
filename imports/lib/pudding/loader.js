84ccf1ec37d6bbc2ddde6a4661f79c5ca4c8f16d
retotrinkler <reto.trinkler@gmail.com>
2016-07-23 12:32:57 +0200

    truffle v2.0.4 etherpudding v3.1.2

--- imports/lib/pudding/loader.js
+++ imports/lib/pudding/loader.js
@@ -1,8 +1,18 @@
-import Pudding from './pudding.js';
+import web3 from '../ethereum/web3.js';
 // Import Truffle file
-import MetaCoin from '../smart-contracts/environments/development/contracts/MetaCoin.sol.js';
+import MetaCoin from '../smart-contracts/build/contracts/MetaCoin.sol.js';
+import ConvertLib from '../smart-contracts/build/contracts/ConvertLib.sol.js';
 
 // Load Truffle file
-MetaCoin.load(Pudding);
-let meta = MetaCoin.deployed();
+MetaCoin.setProvider(web3);
+ConvertLib.setProvider(web3);
+
+// This works!
+var meta = MetaCoin.deployed();
 console.log(meta);
+
+// This doesn't work not!
+const fromAddr =web3.eth.coinbase;
+const gasPrice = 100000000000;
+const gas = 2500000;
+ConvertLib.new({ from: fromAddr, gasPrice: gasPrice, gas: gas });
