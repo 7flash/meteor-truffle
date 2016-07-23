daaeb488e1039a8a7a79550d852413006a58bdf2
retotrinkler <reto.trinkler@gmail.com>
2016-07-23 12:40:37 +0200

    typo

--- imports/lib/pudding/loader.js
+++ imports/lib/pudding/loader.js
@@ -10,7 +10,7 @@ ConvertLib.setProvider(web3);
 var convertLib = ConvertLib.deployed();
 console.log(convertLib);
 
-// This doesn't work not!
+// This does not work!
 const fromAddr =web3.eth.coinbase;
 const gasPrice = 100000000000;
 const gas = 2500000;
